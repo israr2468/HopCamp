4b623712380b78a128afd349fc9e258a41a84a8a
Israr Ali <israrali2468@gmail.com>
2023-05-30 13:06:03 -0700

    added faker generator and seed files

--- HopCamp/k6scriptTest.js
+++ HopCamp/k6scriptTest.js
@@ -1,7 +1,15 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { check, sleep } from "k6";
+
+export const option = {
+  vus: 10,
+  duration: "3m",
+};
 
 export default function () {
-  http.get("http://test.k6.io");
+  let response = http.get("http://test.k6.io");
+  check(response, {
+    "status was 200": (r) => r.status == 200,
+  });
   sleep(1);
 }
